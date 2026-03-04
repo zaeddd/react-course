ab025d8baf06214d3ecf691d4494b2e59535db0c
Zaedrick <zaedalvarico@gmail.com>
2026-03-04 15:04:56 +0800

    z

--- chatbot-project/src/App.jsx
+++ chatbot-project/src/App.jsx
@@ -1,35 +1,106 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useRef, useEffect } from 'react'
+import { ChatInput } from './components/ChatInput.jsx';
+import RobotProfileImage from './assets/robot.png'; 
+import UserProfileImage from './assets/user.png';
+import LoadingSpinner from './assets/loading-spinner.gif';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+function ChatMessage({ message, sender }) {
+  // const message = props.message;
+  // const sender = props.sender;
+  // const { message, sender } = props;
+
+  /*
+  if (sender === 'robot') {
+    return (
+      <div>
+        <img src="robot.png" width="50" />
+        {message}
+      </div>
+    );
+  }
+  */
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className={
+      sender === 'user'
+        ? 'chat-message-user'
+        : 'chat-message-robot'
+    }>
+      {sender === 'robot' && (
+        <img src={RobotProfileImage} className="chat-message-profile" />
+      )}
+      <div className="chat-message-text">
+        {message}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      {sender === 'user' && (
+        <img src={UserProfileImage} className="chat-message-profile" />
+      )}
+    </div>
+  );
+}
+
+function useAutoScroll(chatMessages) {
+  const chatMessagesRef = useRef(null);
+
+  useEffect(() => {
+    const containerElem = chatMessagesRef.current;
+    if (containerElem) {
+      containerElem.scrollTop = containerElem.scrollHeight;
+    }
+  }, [chatMessages]);
+
+  return chatMessagesRef;
+}
+
+function ChatMessages({ chatMessages }) {
+  const chatMessagesRef = useAutoScroll(chatMessages);
+
+  useEffect(() => {
+    const containerElem = chatMessagesRef.current;
+    if (containerElem) {
+      containerElem.scrollTop = containerElem.scrollHeight;
+    }
+  }, [chatMessages]);
+
+  return (
+    <div className="chat-messages-container" ref={chatMessagesRef}>
+      {chatMessages.map((chatMessage) => {
+        return (
+          <ChatMessage
+            message={chatMessage.message}
+            sender={chatMessage.sender}
+            key={chatMessage.id}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+function App() {
+  const [chatMessages, setChatMessages] = useState([]);
+        // const [chatMessages, setChatMessages] = array;
+        // const chatMessages = array[0];
+        // const setChatMessages = array[1];
+
+  return (
+    <div className="app-container">
+      {chatMessages.length === 0 && (
+        <p className="welcome-message">
+          Welcome to the chatbot project! Send a message using the textbox below.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      )}
+        <ChatMessages
+          chatMessages={chatMessages}
+        />
+        <ChatInput
+          chatMessages={chatMessages}
+          setChatMessages={setChatMessages}
+        />
+    </div>
+  );
 }
 
 export default App
---
+++ chatbot-project/src/components/ChatInput.jsx
@@ -0,0 +1,72 @@
+import { useState } from 'react';
+import { Chatbot } from 'supersimpledev';
+
+
+export function ChatInput({ chatMessages, setChatMessages }) {
+  const [inputText, setInputText] = useState('');
+
+  function saveInputText(event) {
+    setInputText(event.target.value);
+  }
+
+  function keyPressFunction(event) {
+      if(event.key === 'Enter'){
+          setInputText('');
+          sendMessage();
+      }else if(event.key === 'Escape'){
+          setInputText('');
+      }
+  };
+
+  async function sendMessage() {
+    const newChatMessages = [
+      ...chatMessages,
+      {
+        message: inputText,
+        sender: 'user',
+        id: crypto.randomUUID()
+      }
+    ];
+
+    setChatMessages([
+      ...newChatMessages,
+      // This creates a temporary Loading... message.
+      // Because we don't save this message in newChatMessages,
+      // it will be removed later, when we add the response.
+      {
+        message: <img src={LoadingSpinner} className="loading-spinner" />,
+        sender: 'robot',
+        id: crypto.randomUUID()
+      }
+    ]);
+
+    const response = await Chatbot.getResponseAsync(inputText);
+    setChatMessages([
+      ...newChatMessages,
+      {
+        message: response,
+        sender: 'robot',
+        id: crypto.randomUUID()
+      }
+    ]);
+
+    setInputText('');
+  }
+
+  return (
+    <div className="chat-input-container">
+      <input
+        placeholder="Send a message to Chatbot"
+        size="30"
+        onChange={saveInputText}
+        onKeyDown={keyPressFunction}
+        value={inputText}
+        className="chat-input"
+      />
+      <button
+        onClick={sendMessage}
+        className="send-button"
+      >Send</button>
+    </div>
+  );
+}
